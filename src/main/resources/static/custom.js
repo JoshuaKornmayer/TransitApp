68ecb6b038a5c6f637254d658936b1f8e9008771
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-08-04 15:30:49 -0400

    added my location to the map

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,4 +1,4 @@
-var map;
+var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {
@@ -8,6 +8,7 @@ function initMap() {
     zoom: 15,
     scrollwheel: false,
   });
+  infoWindow = new google.maps.InfoWindow();
 
   for (i = 0; i < busLocations.length; i++) {
     var marker = new google.maps.Marker({
@@ -18,4 +19,37 @@ function initMap() {
       map: map,
     });
   }
+}
+
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent("Location found.");
+      infoWindow.open(map);
+      map.setCenter(pos);
+    },
+    function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    }
+  );
+} else {
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
